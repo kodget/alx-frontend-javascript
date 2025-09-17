da4d9f4801f139e8f4e2866dd5f78b35920fc7f0
Muhammad <muminmuhsina1@gmail.com>
2025-09-17 07:18:52 -0700

    refractor: fix function call

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -7,14 +7,14 @@
 import { RowElement, RowID } from "./interface";
 import * as CRUD from "./crud";
 
-const row: RowElement = {
+let row: RowElement = {
   firstName: "Guillaume",
   lastName: "Salve",
 };
 let rowID: RowID = 125;
 
 const newRowID: RowID = CRUD.insertRow(row);
-row.age = 23;
+row = { ...row, age: 23 };
 const updatedRow: RowElement = CRUD.updateRow(rowID, row);
 CRUD.updateRow(newRowID, updatedRow);
 CRUD.deleteRow(newRowID);
